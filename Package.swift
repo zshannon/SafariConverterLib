4b2093198266decf12456bc11c1e83637464e4cb
Zane Shannon <z@zcs.me>
2024-04-25 15:17:01 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,23 +10,19 @@ let package = Package(
         .library(
             name: "ContentBlockerConverter",
             type: .static,
-            targets: ["ContentBlockerConverter", "ContentBlockerEngine"]),
-        .executable(
-            name: "ConverterTool",
-            targets: ["CommandLineWrapper"])
+            targets: ["ContentBlockerConverter", "ContentBlockerEngine"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/gumob/PunycodeSwift.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "CommandLineWrapper",
-            dependencies: ["ContentBlockerConverter", "Shared", "ArgumentParser"]),
+        // .target(
+        //     name: "CommandLineWrapper",
+        //     dependencies: ["ContentBlockerConverter", "Shared", "ArgumentParser"]),
         .target(
             name: "ContentBlockerConverter",
             dependencies: ["Punnycode", "Shared"]),
